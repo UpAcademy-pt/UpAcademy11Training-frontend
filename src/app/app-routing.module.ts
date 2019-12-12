1378a34ae653c8695e31ae9bd57e2969153dbe18
Francisco Freire <franciscofreire1992@gmail.com>
2019-12-12 11:53:00 +0000

    --commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login-view/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { AppComponent } from './app.component';
 import { AuthGuard } from './core/guards/auth.guard';
 
 
