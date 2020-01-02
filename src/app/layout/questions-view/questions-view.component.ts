60d7714e08b5b819065bdb695661f31ee83a4671
pmgaspar1988 <>
2020-01-02 12:07:08 +0000

    questionario html

--- src/app/layout/questions-view/questions-view.component.ts
+++ src/app/layout/questions-view/questions-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SessionServiceService } from 'src/app/core/services/user-service/session-service.service';
+import { UserServiceService } from 'src/app/core/services/user-service/user-service.service';
 
 @Component({
   selector: 'app-questions-view',
