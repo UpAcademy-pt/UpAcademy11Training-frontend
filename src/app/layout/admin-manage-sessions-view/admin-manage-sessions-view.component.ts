98299187ab782cef6d73f9caeaf42c7c43af616b
pmgaspar1988 <>
2020-01-05 16:00:43 +0000

    delete user, session card collapsed,

--- src/app/layout/admin-manage-sessions-view/admin-manage-sessions-view.component.ts
+++ src/app/layout/admin-manage-sessions-view/admin-manage-sessions-view.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { SessionServiceService } from 'src/app/core/services/user-service/session-service.service';
+import { UserServiceService } from 'src/app/core/services/user-service/user-service.service';
+import { User } from 'src/app/core/models/user';
+
 
 @Component({
   selector: 'app-admin-manage-sessions-view',
@@ -17,15 +20,28 @@ export class AdminManageSessionsViewComponent implements OnInit {
   private duration = '';
   private instructor = 0;
 
-  constructor(config: NgbModalConfig, 
-              private modalService: NgbModal, 
-              private sessionService: SessionServiceService) {
+  constructor(config: NgbModalConfig,
+    private modalService: NgbModal,
+    private sessionService: SessionServiceService,
+    private userService: UserServiceService) {
     // customize default values of modals used by this component tree
     config.backdrop = 'static';
     config.keyboard = false;
   }
+  users: User[] = [];
+
 
   ngOnInit() {
+    this.userService.getAllUsers().subscribe((data: User[]) => {
+      this.users = data;
+
+    });
+  }
+
+  selectedDeviceObj = this.users[1];
+
+  onChangeObj(newObj) {
+    this.selectedDeviceObj = newObj;
   }
 
   open(content) {
@@ -33,20 +49,22 @@ export class AdminManageSessionsViewComponent implements OnInit {
   }
 
   create() {
-    let date = this.sessionDate.replace("T"," ");
+    let date = this.sessionDate.replace("T", " ");
     console.log(this.sessionDate);
-    
-    this.sessionService.createSession(this.title, this.location, date, this.capacity, 
-    this.requirements, this.duration, this.instructor).subscribe(data => {
-    this.title = '';
-    this.location = '';
-    this.sessionDate = '';
-    this.capacity = 0;
-    this.requirements = '';
-    this.duration = '';
-    this.instructor = 0;
-  });
+    this.sessionService.createSession(this.title, this.location, date, this.capacity,
+      this.requirements, this.duration, this.instructor).subscribe(data => {
+        this.title = '';
+        this.location = '';
+        this.sessionDate = '';
+        this.capacity = 0;
+        this.requirements = '';
+        this.duration = '';
+        this.instructor = 0;
+      });
+  }
+
 }
-}
+
+
 
 
